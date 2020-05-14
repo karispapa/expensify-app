c4faa067e632e842b3fae3a206cbe3c40349dce8
karispapa <skariuki91@gmail.com>
2020-05-14 08:34:55 +0300

    Formated date and amount in ExpenseList file

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import moment from 'moment';
+import numeral from 'numeral';
+
+
 // import { connect } from 'react-redux';
 // import { removeExpense } from '../actions/expenses'
 
@@ -10,7 +14,13 @@ const ExportListItem = ({id, description, amount, createdAt})=>(
      <h3>{description} </h3>
     </Link>
     
-    <p> {createdAt} ksh: {amount} </p>
+    <p> 
+    
+      {numeral(amount).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')} 
+  
+     </p>
   </div>
 );
 
